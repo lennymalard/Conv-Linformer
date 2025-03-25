c09a021b470c3938cbd3a764dd6a87407a4a5f82
lennymalard <lennymalard@gmail.com>
2025-03-25 18:35:14 +0100

    First convolutional linformer implementation

--- conv_linformer/conv_linformer.py
+++ conv_linformer/conv_linformer.py
@@ -96,6 +96,7 @@ class FeedForward(nn.Module):
         return x
 
 class ConvLinformerSelfAttention(nn.Module):
+    #TODO Merge it with LinformerSelfAttention
     def __init__(self, dim, seq_len, k = 256, heads = 8, dim_head = None, one_kv_head = False, share_kv = False, dropout = 0.):
         super().__init__()
         assert (dim % heads) == 0, 'dimension must be divisible by the number of heads'
@@ -113,20 +114,22 @@ class ConvLinformerSelfAttention(nn.Module):
         kv_dim = dim_head if one_kv_head else (dim_head * heads)
         self.to_k = nn.Linear(dim, kv_dim, bias = False)
         self.proj_k = nn.Conv1d(
-            in_channels=seq_len,
-            out_channels=k,
-            kernel_size=32,
-            padding='same'
+            in_channels=dim,
+            out_channels=dim,
+            kernel_size=seq_len//k,
+            stride=seq_len//k,
+            bias=False
         )
 
         self.share_kv = share_kv
         if not share_kv:
             self.to_v = nn.Linear(dim, kv_dim, bias = False)
             self.proj_v = nn.Conv1d(
-                in_channels=256,
-                out_channels=64,
-                kernel_size=32,
-                padding='same'
+                in_channels=dim,
+                out_channels=dim,
+                kernel_size=seq_len//k,
+                stride=seq_len//k,
+                bias=False
             )
 
         self.dropout = nn.Dropout(dropout)
@@ -147,29 +150,85 @@ class ConvLinformerSelfAttention(nn.Module):
 
         kv_projs = [self.proj_k, self.proj_v if not self.share_kv else self.proj_k]
 
-        # allow for variable sequence lengths (less than maximum sequence length) by slicing projections
         if kv_len < self.seq_len:
             kv_projs = [deepcopy(proj) for proj in kv_projs]
             kv_projs[0].weight = kv_projs[0].weight[:, :kv_len]
             kv_projs[1].weight = kv_projs[1].weight[:, :kv_len]
 
-        # project keys and values along the sequence length dimension to k
-        keys = kv_projs[0](keys)
-        values = kv_projs[1](values)
+        keys = kv_projs[0](keys.transpose(-1,-2)).transpose(-1,-2)
+        values = kv_projs[1](values.transpose(-1,-2)).transpose(-1,-2)
 
-        # merge head into batch for queries and key / values
         queries = queries.reshape(b, n, h, -1).transpose(1, 2)
 
         merge_key_values = lambda t: t.reshape(b, k, -1, d_h).transpose(1, 2).expand(-1, h, -1, -1)
         keys, values = map(merge_key_values, (keys, values))
 
-        # attention
         dots = torch.einsum('bhnd,bhkd->bhnk', queries, keys) * (d_h ** -0.5)
         attn = dots.softmax(dim=-1)
         attn = self.dropout(attn)
         out = torch.einsum('bhnk,bhkd->bhnd', attn, values)
 
-        # split heads
+        out = out.transpose(1, 2).reshape(b, n, -1)
+        return self.to_out(out)
+
+class LinformerSelfAttention(nn.Module):
+    def __init__(self, dim, seq_len, k = 256, heads = 8, dim_head = None, one_kv_head = False, share_kv = False, dropout = 0.):
+        super().__init__()
+        assert (dim % heads) == 0, 'dimension must be divisible by the number of heads'
+
+        self.seq_len = seq_len
+        self.k = k
+
+        self.heads = heads
+
+        dim_head = default(dim_head, dim // heads)
+        self.dim_head = dim_head
+
+        self.to_q = nn.Linear(dim, dim_head * heads, bias = False)
+
+        kv_dim = dim_head if one_kv_head else (dim_head * heads)
+        self.to_k = nn.Linear(dim, kv_dim, bias = False)
+        self.proj_k = nn.Parameter(init_(torch.zeros(seq_len, k)))
+
+        self.share_kv = share_kv
+        if not share_kv:
+            self.to_v = nn.Linear(dim, kv_dim, bias = False)
+            self.proj_v = nn.Parameter(init_(torch.zeros(seq_len, k)))
+
+        self.dropout = nn.Dropout(dropout)
+        self.to_out = nn.Linear(dim_head * heads, dim)
+
+    def forward(self, x, context = None, **kwargs):
+        b, n, d, d_h, h, k = *x.shape, self.dim_head, self.heads, self.k
+
+        kv_len = n if context is None else context.shape[1]
+        assert kv_len <= self.seq_len, f'the sequence length of the key / values must be {self.seq_len} - {kv_len} given'
+
+        queries = self.to_q(x)
+
+        proj_seq_len = lambda args: torch.einsum('bnd,nk->bkd', *args)
+
+        kv_input = x if context is None else context
+
+        keys = self.to_k(kv_input)
+        values = self.to_v(kv_input) if not self.share_kv else keys
+
+        kv_projs = (self.proj_k, self.proj_v if not self.share_kv else self.proj_k)
+
+        if kv_len < self.seq_len:
+            kv_projs = map(lambda t: t[:kv_len], kv_projs)
+
+        keys, values = map(proj_seq_len, zip((keys, values), kv_projs))
+        queries = queries.reshape(b, n, h, -1).transpose(1, 2)
+
+        merge_key_values = lambda t: t.reshape(b, k, -1, d_h).transpose(1, 2).expand(-1, h, -1, -1)
+        keys, values = map(merge_key_values, (keys, values))
+
+        dots = torch.einsum('bhnd,bhkd->bhnk', queries, keys) * (d_h ** -0.5)
+        attn = dots.softmax(dim=-1)
+        attn = self.dropout(attn)
+        out = torch.einsum('bhnk,bhkd->bhnd', attn, values)
+
         out = out.transpose(1, 2).reshape(b, n, -1)
         return self.to_out(out)
 
@@ -177,7 +236,15 @@ class ConvLinformer(nn.Module):
     def __init__(self, dim, seq_len, depth, k = 256, heads = 8, dim_head = None, one_kv_head = False, share_kv = False, reversible = False, dropout = 0.):
         super().__init__()
         layers = nn.ModuleList([])
-        for _ in range(depth):
+        for _ in range(depth//2):
+            attn = LinformerSelfAttention(dim, seq_len, k = k, heads = heads, dim_head = dim_head, one_kv_head = one_kv_head, share_kv = share_kv, dropout = dropout)
+            ff = FeedForward(dim, dropout = dropout)
+
+            layers.append(nn.ModuleList([
+                PreNorm(dim, attn),
+                PreNorm(dim, ff)
+            ]))
+        for _ in range(depth//2, depth):
             attn = ConvLinformerSelfAttention(dim, seq_len, k = k, heads = heads, dim_head = dim_head, one_kv_head = one_kv_head, share_kv = share_kv, dropout = dropout)
             ff = FeedForward(dim, dropout = dropout)
 
