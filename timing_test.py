f683f5007bd108141e38ffd43f224c89e8b74738
lennymalard <lennymalard@gmail.com>
2025-04-05 18:01:42 +0200

    Minor changes

--- timing_test.py
+++ timing_test.py
@@ -5,9 +5,7 @@ from conv_linformer.conv_linformer import *
 import torch
 from time import time
 
-seq_len = 4096
-
-dummy_data = torch.randint(0, 30000, (1, seq_len))
+sequence_lengths = [512, 1024, 2048, 4096]
 
 transformer_config = TransformerConfig(
             num_tokens=30000,
@@ -20,7 +18,7 @@ transformer_config = TransformerConfig(
 linformer_config = LinformerConfig(
             num_tokens=30000,
             dim=512,
-            seq_len=seq_len,
+            seq_len=4096,
             depth=8,
             k = 256,
             heads = 8,
@@ -29,7 +27,7 @@ linformer_config = LinformerConfig(
 conv_linformer_config = ConvLinformerConfig(
             num_tokens=30000,
             dim=512,
-            seq_len=seq_len,
+            seq_len=4096,
             depth=8,
             k = 256,
             heads = 8,
@@ -40,14 +38,23 @@ transformer = TransformerLM(transformer_config)
 linformer = LinformerLM(linformer_config)
 conv_linformer = ConvLinformerLM(conv_linformer_config)
 
-ti = time()
-transformer(dummy_data)
-print(time() - ti)
+transformer.eval()
+linformer.eval()
+conv_linformer.eval()
 
-ti = time()
-linformer(dummy_data)
-print(time() - ti)
+for seq_len in sequence_lengths:
+    dummy_data = torch.randint(0, 30000, (1, seq_len))
 
-ti = time()
-conv_linformer(dummy_data)
-print(time() - ti)
+    ti = time()
+    transformer(dummy_data)
+    print(f"Transformer took {(time() - ti):.3f} seconds for a sequence of {seq_len} tokens.")
+
+    ti = time()
+    linformer(dummy_data)
+    print(f"Linformer took {(time() - ti):.3f} seconds for a sequence of {seq_len} tokens.")
+
+    ti = time()
+    conv_linformer(dummy_data)
+    print(f"Conv Linformer took {(time() - ti):.3f} seconds for a sequence of {seq_len} tokens.")
+
+    print("\n")
